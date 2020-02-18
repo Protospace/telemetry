7a95135a6374440b42541942c522100167427ca1
Tanner Collin <git@tannercollin.com>
2020-02-17 20:23:50 -0700

    Get temperature data from sensor

--- sht30_temperature/sht30_temperature.ino
+++ sht30_temperature/sht30_temperature.ino
@@ -4,8 +4,10 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
+#include <WEMOS_SHT3X.h>
 
 ESP8266WiFiMulti WiFiMulti;
+SHT3X sht30(0x45);
 
 void setup() {
 
@@ -38,23 +40,31 @@ void loop() {
 
 		Serial.print("[HTTPS] begin...\n");
 		if (https.begin(*client, "https://api.my.protospace.ca/stats/bay_108_temp/")) {
-			Serial.print("[HTTPS] POST...\n");
-			// start connection and send HTTP header
-			https.addHeader("Content-Type", "application/x-www-form-urlencoded");
-			int httpCode = https.POST("data=1234");
+			Serial.print("Get temperature data...\n");
 
-			// httpCode will be negative on error
-			if (httpCode > 0) {
-				// HTTP header has been send and Server response header has been handled
-				Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+			if (sht30.get() == 0) {
+				Serial.print("[HTTPS] POST...\n");
 
-				// file found at server
-				if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-					String payload = https.getString();
-					Serial.println(payload);
+				// start connection and send HTTP header
+				https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+				int httpCode = https.POST("data=" + String(sht30.cTemp));
+
+				// httpCode will be negative on error
+				if (httpCode > 0) {
+					// HTTP header has been send and Server response header has been handled
+					Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+
+					// file found at server
+					if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+						String payload = https.getString();
+						Serial.println(payload);
+					}
+				} else {
+					Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
 				}
+
 			} else {
-				Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+				Serial.printf("Temperature data failed.\n");
 			}
 
 			https.end();
