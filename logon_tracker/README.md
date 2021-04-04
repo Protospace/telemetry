1eb903e8799b0743c96a49acb4755b2f1d8ed927
Tanner Collin <git@tannercollin.com>
2021-04-03 18:52:52 -0600

    Update logon tracker

---
+++ logon_tracker/README.md
@@ -0,0 +1,7 @@
+# Protospace Logon Tracker
+
+Tracks users who log onto a certain computer and POSTs it to the portal.
+
+Windows 7 file location:
+
+`C:\ProgramData\Microsoft\Windows\Start Menu\Programs`
--- logon_tracker/logon_tracker.ps1
+++ logon_tracker/logon_tracker.ps1
@@ -1,8 +1,14 @@
 $postParams = @{name=$env:computername ;username=$env:UserName}
+
 while($true)
 {
+	if (!([System.Diagnostics.Process]::GetProcessesByName("logonui")))
+	{
+		# screen is unlocked
 
-# Invoke-WebRequest -Uri https://api.spaceport.dns.t0.vc/stats/track/ -Method POST -Body $postParams -ContentType application/x-www-form-urlencoded -UseBasicParsing
-  Invoke-WebRequest -Uri https://api.my.protospace.ca/stats/track/ -Method POST -Body $postParams -ContentType application/x-www-form-urlencoded -UseBasicParsing
-Start-Sleep -s 10
+		# Invoke-WebRequest -Uri https://api.spaceport.dns.t0.vc/stats/track/ -Method POST -Body $postParams -ContentType application/x-www-form-urlencoded -UseBasicParsing
+		Invoke-WebRequest -Uri https://api.my.protospace.ca/stats/track/ -Method POST -Body $postParams -ContentType application/x-www-form-urlencoded -UseBasicParsing
+	}
+
+	Start-Sleep -s 10
 }
