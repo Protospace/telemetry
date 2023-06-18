d65926957cc6cd7f7a25569b748c13fcb107016b
Tanner Collin <git@tannercollin.com>
2023-06-18 12:45:50 -0600

    Switch alarm_tracker to dscKeybusInterface example

--- alarm_tracker/alarm_tracker.ino
+++ alarm_tracker/alarm_tracker.ino
@@ -1,85 +1,398 @@
-#include <Arduino.h>
+/*
+ *  Pushbullet Push Notification 1.5 (esp8266)
+ *
+ *  Processes the security system status and demonstrates how to send a push notification when the status has changed.
+ *  This example sends notifications via Pushbullet: https://www.pushbullet.com
+ *
+ *  Usage:
+ *    1. Set the WiFi SSID and password in the sketch.
+ *    2. Create a PushBullet API access token: https://www.pushbullet.com/#settings
+ *    3. Copy the access token to pushbulletToken.
+ *    4. Upload the sketch.
+ *
+ *  Release notes:
+ *    1.5 - Update HTTPS root certificate for api.pushbullet.com
+ *          Added DSC Classic series support
+ *    1.4 - Add HTTPS certificate validation, add customizable message prefix
+ *    1.3 - Updated esp8266 wiring diagram for 33k/10k resistors
+ *    1.2 - Check if WiFi disconnects and wait to send updates until reconnection
+ *          Add appendPartition() to simplify sketch
+ *          esp8266 Arduino Core version check for BearSSL
+ *    1.1 - Set authentication method for BearSSL in esp8266 Arduino Core 2.5.0+
+ *          Added notifications - Keybus connected, armed status, zone alarm status
+ *    1.0 - Initial release
+ *
+ *  Wiring:
+ *      DSC Aux(+) --- 5v voltage regulator --- esp8266 development board 5v pin (NodeMCU, Wemos)
+ *
+ *      DSC Aux(-) --- esp8266 Ground
+ *
+ *                                         +--- dscClockPin  // Default: D1, GPIO 5
+ *      DSC Yellow --- 33k ohm resistor ---|
+ *                                         +--- 10k ohm resistor --- Ground
+ *
+ *                                         +--- dscReadPin  // Default: D2, GPIO 4
+ *      DSC Green ---- 33k ohm resistor ---|
+ *                                         +--- 10k ohm resistor --- Ground
+ *
+ *      Classic series only, PGM configured for PC-16 output:
+ *      DSC PGM ---+-- 1k ohm resistor --- DSC Aux(+)
+ *                 |
+ *                 |                       +--- dscPC16Pin   // Default: D7, GPIO 13
+ *                 +-- 33k ohm resistor ---|
+ *                                         +--- 10k ohm resistor --- Ground
+ *
+ *  Issues and (especially) pull requests are welcome:
+ *  https://github.com/taligentx/dscKeybusInterface
+ *
+ *  This example code is in the public domain.
+ */
+
+// DSC Classic series: uncomment for PC1500/PC1550 support (requires PC16-OUT configuration per README.md)
+//#define dscClassicSeries
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClientSecureBearSSL.h>
+#include <dscKeybusInterface.h>
 
-#define NUM_SAMPLES 100
+// Settings
+const char* wifiSSID = "";
+const char* wifiPassword = "";
+const char* pushbulletToken = "";  // Set the access token generated in the Pushbullet account settings
+const char* messagePrefix = "[Security system] ";  // Set a prefix for all messages
 
-ESP8266WiFiMulti WiFiMulti;
+// Configures the Keybus interface with the specified pins.
+#define dscClockPin D1  // GPIO 5
+#define dscReadPin  D2  // GPIO 4
+#define dscPC16Pin  D7  // DSC Classic Series only, GPIO 13
+
+// HTTPS root certificate for api.pushbullet.com: Google Trust Services GTS Root R1, expires 2036.06.21
+const char pushbulletCertificateRoot[] = R"=EOF=(
+-----BEGIN CERTIFICATE-----
+MIIFVzCCAz+gAwIBAgINAgPlk28xsBNJiGuiFzANBgkqhkiG9w0BAQwFADBHMQsw
+CQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzEU
+MBIGA1UEAxMLR1RTIFJvb3QgUjEwHhcNMTYwNjIyMDAwMDAwWhcNMzYwNjIyMDAw
+MDAwWjBHMQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZp
+Y2VzIExMQzEUMBIGA1UEAxMLR1RTIFJvb3QgUjEwggIiMA0GCSqGSIb3DQEBAQUA
+A4ICDwAwggIKAoICAQC2EQKLHuOhd5s73L+UPreVp0A8of2C+X0yBoJx9vaMf/vo
+27xqLpeXo4xL+Sv2sfnOhB2x+cWX3u+58qPpvBKJXqeqUqv4IyfLpLGcY9vXmX7w
+Cl7raKb0xlpHDU0QM+NOsROjyBhsS+z8CZDfnWQpJSMHobTSPS5g4M/SCYe7zUjw
+TcLCeoiKu7rPWRnWr4+wB7CeMfGCwcDfLqZtbBkOtdh+JhpFAz2weaSUKK0Pfybl
+qAj+lug8aJRT7oM6iCsVlgmy4HqMLnXWnOunVmSPlk9orj2XwoSPwLxAwAtcvfaH
+szVsrBhQf4TgTM2S0yDpM7xSma8ytSmzJSq0SPly4cpk9+aCEI3oncKKiPo4Zor8
+Y/kB+Xj9e1x3+naH+uzfsQ55lVe0vSbv1gHR6xYKu44LtcXFilWr06zqkUspzBmk
+MiVOKvFlRNACzqrOSbTqn3yDsEB750Orp2yjj32JgfpMpf/VjsPOS+C12LOORc92
+wO1AK/1TD7Cn1TsNsYqiA94xrcx36m97PtbfkSIS5r762DL8EGMUUXLeXdYWk70p
+aDPvOmbsB4om3xPXV2V4J95eSRQAogB/mqghtqmxlbCluQ0WEdrHbEg8QOB+DVrN
+VjzRlwW5y0vtOUucxD/SVRNuJLDWcfr0wbrM7Rv1/oFB2ACYPTrIrnqYNxgFlQID
+AQABo0IwQDAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4E
+FgQU5K8rJnEaK0gnhS9SZizv8IkTcT4wDQYJKoZIhvcNAQEMBQADggIBAJ+qQibb
+C5u+/x6Wki4+omVKapi6Ist9wTrYggoGxval3sBOh2Z5ofmmWJyq+bXmYOfg6LEe
+QkEzCzc9zolwFcq1JKjPa7XSQCGYzyI0zzvFIoTgxQ6KfF2I5DUkzps+GlQebtuy
+h6f88/qBVRRiClmpIgUxPoLW7ttXNLwzldMXG+gnoot7TiYaelpkttGsN/H9oPM4
+7HLwEXWdyzRSjeZ2axfG34arJ45JK3VmgRAhpuo+9K4l/3wV3s6MJT/KYnAK9y8J
+ZgfIPxz88NtFMN9iiMG1D53Dn0reWVlHxYciNuaCp+0KueIHoI17eko8cdLiA6Ef
+MgfdG+RCzgwARWGAtQsgWSl4vflVy2PFPEz0tv/bal8xa5meLMFrUKTX5hgUvYU/
+Z6tGn6D/Qqc6f1zLXbBwHSs09dR2CQzreExZBfMzQsNhFRAbd03OIozUhfJFfbdT
+6u9AWpQKXCBfTkBdYiJ23//OYb2MI3jSNwLgjt7RETeJ9r/tSQdirpLsQBqvFAnZ
+0E6yove+7u7Y/9waLd64NnHi/Hm3lCXRSHNboTXns5lndcEZOitHTtNCjv0xyBZm
+2tIMPNuzjsmhDYAPexZ3FL//2wmUspO8IFgV6dtxQ/PeEMMA3KgqlbbC1j+Qa3bb
+bP6MvPJwNQzcmRk13NfIRmPVNnGuV/u3gm3c
+-----END CERTIFICATE-----
+)=EOF=";
+
+// Initialize components
+#ifndef dscClassicSeries
+dscKeybusInterface dsc(dscClockPin, dscReadPin);
+#else
+dscClassicInterface dsc(dscClockPin, dscReadPin, dscPC16Pin);
+#endif
+X509List pushbulletCert(pushbulletCertificateRoot);
+WiFiClientSecure ipClient;
+bool wifiConnected = true;
+
 
 void setup() {
-
 	Serial.begin(115200);
-	// Serial.setDebugOutput(true);
-
+	delay(1000);
 	Serial.println();
 	Serial.println();
-	Serial.println("Serial started");
 
+	Serial.print(F("WiFi...."));
 	WiFi.mode(WIFI_STA);
-	WiFiMulti.addAP("Protospace", "yycmakers");
+	WiFi.begin(wifiSSID, wifiPassword);
+	ipClient.setTrustAnchors(&pushbulletCert);
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(500);
+	}
+	Serial.print(F("connected: "));
+	Serial.println(WiFi.localIP());
 
-	pinMode(A0, INPUT);
+	Serial.print(F("NTP time...."));
+	configTime(0, 0, "pool.ntp.org");
+	time_t now = time(nullptr);
+	while (now < 24 * 3600)
+	{
+		Serial.print(".");
+		delay(2000);
+		now = time(nullptr);
+	}
+	Serial.println(F("synchronized."));
 
-	delay(5000);
+	// Sends a message on startup to verify connectivity
+	Serial.print(F("Pushbullet...."));
+	if (sendMessage("Initializing")) Serial.println(F("connected."));
+	else Serial.println(F("connection error."));
+
+	// Starts the Keybus interface
+	dsc.begin();
+	Serial.println(F("DSC Keybus Interface is online."));
 }
 
+
 void loop() {
-	// wait for WiFi connection
-	if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-		std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+	// Updates status if WiFi drops and reconnects
+	if (!wifiConnected && WiFi.status() == WL_CONNECTED) {
+		Serial.println("WiFi reconnected");
+		wifiConnected = true;
+		dsc.pauseStatus = false;
+		dsc.statusChanged = true;
+	}
+	else if (WiFi.status() != WL_CONNECTED && wifiConnected) {
+		Serial.println("WiFi disconnected");
+		wifiConnected = false;
+		dsc.pauseStatus = true;
+	}
 
-		client->setInsecure();
+	dsc.loop();
 
-		HTTPClient https;
+	if (dsc.statusChanged) {      // Checks if the security system status has changed
+		dsc.statusChanged = false;  // Reset the status tracking flag
 
-		Serial.print("[HTTPS] begin...\n");
-		if (https.begin(*client, "https://api.my.protospace.ca/stats/alarm/")) {
-			Serial.print("[DATA] Get light data...\n");
-
-			long total = 0;
-
-			for (int i=0; i < NUM_SAMPLES; i++) {
-				total += analogRead(A0);
-				delay(10);
-			}
-
-			total /= NUM_SAMPLES;
-
-			Serial.printf("[DATA] Average value read: %d\n", total);
-
-			Serial.print("[HTTPS] POST...\n");
-
-			// start connection and send HTTP header
-			https.addHeader("Content-Type", "application/x-www-form-urlencoded");
-			int httpCode = https.POST("data=" + String(total));
-
-			// httpCode will be negative on error
-			if (httpCode > 0) {
-				// HTTP header has been send and Server response header has been handled
-				Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
-
-				// file found at server
-				if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-					String payload = https.getString();
-					Serial.println(payload);
-				}
-			} else {
-				Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
-			}
-
-			https.end();
-		} else {
-			Serial.printf("[HTTPS] Unable to connect\n");
+		// If the Keybus data buffer is exceeded, the sketch is too busy to process all Keybus commands.  Call
+		// loop() more often, or increase dscBufferSize in the library: src/dscKeybus.h or src/dscClassic.h
+		if (dsc.bufferOverflow) {
+			Serial.println(F("Keybus buffer overflow"));
+			dsc.bufferOverflow = false;
 		}
 
-		Serial.println("Waiting 60s");
-		delay(60*1000);
-	} else {
-		Serial.println("[WIFI] Error, wait 10s before trying again...");
-		delay(10000);
+		// Checks if the interface is connected to the Keybus
+		if (dsc.keybusChanged) {
+			dsc.keybusChanged = false;  // Resets the Keybus data status flag
+			if (dsc.keybusConnected) sendMessage("Connected");
+			else sendMessage("Disconnected");
+		}
+
+		// Checks status per partition
+		for (byte partition = 0; partition < dscPartitions; partition++) {
+
+			// Skips processing if the partition is disabled or in installer programming
+			if (dsc.disabled[partition]) continue;
+
+			// Checks armed status
+			if (dsc.armedChanged[partition]) {
+				if (dsc.armed[partition]) {
+					char messageContent[30];
+
+					if (dsc.armedAway[partition] && dsc.noEntryDelay[partition]) strcpy(messageContent, "Armed night away: Partition ");
+					else if (dsc.armedAway[partition]) strcpy(messageContent, "Armed away: Partition ");
+					else if (dsc.armedStay[partition] && dsc.noEntryDelay[partition]) strcpy(messageContent, "Armed night stay: Partition ");
+					else if (dsc.armedStay[partition]) strcpy(messageContent, "Armed stay: Partition ");
+
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+				else {
+					char messageContent[22] = "Disarmed: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+			}
+
+			// Checks exit delay status
+			if (dsc.exitDelayChanged[partition]) {
+				dsc.exitDelayChanged[partition] = false;  // Resets the exit delay status flag
+
+				if (dsc.exitDelay[partition]) {
+					char messageContent[36] = "Exit delay in progress: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+				else if (!dsc.exitDelay[partition] && !dsc.armed[partition]) {
+					char messageContent[22] = "Disarmed: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+			}
+
+			// Checks alarm triggered status
+			if (dsc.alarmChanged[partition]) {
+				dsc.alarmChanged[partition] = false;  // Resets the partition alarm status flag
+
+				if (dsc.alarm[partition]) {
+					char messageContent[19] = "Alarm: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+				else if (!dsc.armedChanged[partition]) {
+					char messageContent[22] = "Disarmed: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+			}
+			dsc.armedChanged[partition] = false;  // Resets the partition armed status flag
+
+			// Checks fire alarm status
+			if (dsc.fireChanged[partition]) {
+				dsc.fireChanged[partition] = false;  // Resets the fire status flag
+
+				if (dsc.fire[partition]) {
+					char messageContent[24] = "Fire alarm: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+				else {
+					char messageContent[33] = "Fire alarm restored: Partition ";
+					appendPartition(partition, messageContent);  // Appends the message with the partition number
+					sendMessage(messageContent);
+				}
+			}
+		}
+
+		// Checks for zones in alarm
+		// Zone alarm status is stored in the alarmZones[] and alarmZonesChanged[] arrays using 1 bit per zone, up to 64 zones
+		//   alarmZones[0] and alarmZonesChanged[0]: Bit 0 = Zone 1 ... Bit 7 = Zone 8
+		//   alarmZones[1] and alarmZonesChanged[1]: Bit 0 = Zone 9 ... Bit 7 = Zone 16
+		//   ...
+		//   alarmZones[7] and alarmZonesChanged[7]: Bit 0 = Zone 57 ... Bit 7 = Zone 64
+		if (dsc.alarmZonesStatusChanged) {
+			dsc.alarmZonesStatusChanged = false;                           // Resets the alarm zones status flag
+			for (byte zoneGroup = 0; zoneGroup < dscZones; zoneGroup++) {
+				for (byte zoneBit = 0; zoneBit < 8; zoneBit++) {
+					if (bitRead(dsc.alarmZonesChanged[zoneGroup], zoneBit)) {  // Checks an individual alarm zone status flag
+						bitWrite(dsc.alarmZonesChanged[zoneGroup], zoneBit, 0);  // Resets the individual alarm zone status flag
+						if (bitRead(dsc.alarmZones[zoneGroup], zoneBit)) {       // Zone alarm
+							char messageContent[15] = "Zone alarm: ";
+							char zoneNumber[3];
+							itoa((zoneBit + 1 + (zoneGroup * 8)), zoneNumber, 10); // Determines the zone number
+							strcat(messageContent, zoneNumber);
+							sendMessage(messageContent);
+						}
+						else {
+							char messageContent[24] = "Zone alarm restored: ";
+							char zoneNumber[3];
+							itoa((zoneBit + 1 + (zoneGroup * 8)), zoneNumber, 10); // Determines the zone number
+							strcat(messageContent, zoneNumber);
+							sendMessage(messageContent);
+						}
+					}
+				}
+			}
+		}
+
+		// Checks trouble status
+		if (dsc.troubleChanged) {
+			dsc.troubleChanged = false;  // Resets the trouble status flag
+			if (dsc.trouble) sendMessage("Trouble status on");
+			else sendMessage("Trouble status restored");
+		}
+
+		// Checks for AC power status
+		if (dsc.powerChanged) {
+			dsc.powerChanged = false;  // Resets the battery trouble status flag
+			if (dsc.powerTrouble) sendMessage("AC power trouble");
+			else sendMessage("AC power restored");
+		}
+
+		// Checks panel battery status
+		if (dsc.batteryChanged) {
+			dsc.batteryChanged = false;  // Resets the battery trouble status flag
+			if (dsc.batteryTrouble) sendMessage("Panel battery trouble");
+			else sendMessage("Panel battery restored");
+		}
+
+		// Checks for keypad fire alarm status
+		if (dsc.keypadFireAlarm) {
+			dsc.keypadFireAlarm = false;  // Resets the keypad fire alarm status flag
+			sendMessage("Keypad Fire alarm");
+		}
+
+		// Checks for keypad aux auxiliary alarm status
+		if (dsc.keypadAuxAlarm) {
+			dsc.keypadAuxAlarm = false;  // Resets the keypad auxiliary alarm status flag
+			sendMessage("Keypad Aux alarm");
+		}
+
+		// Checks for keypad panic alarm status
+		if (dsc.keypadPanicAlarm) {
+			dsc.keypadPanicAlarm = false;  // Resets the keypad panic alarm status flag
+			sendMessage("Keypad Panic alarm");
+		}
 	}
+}
+
+
+bool sendMessage(const char* messageContent) {
+
+	// Connects and sends the message as a Pushbullet note-type push
+	if (!ipClient.connect("api.pushbullet.com", 443)) return false;
+	ipClient.println(F("POST /v2/pushes HTTP/1.1"));
+	ipClient.println(F("Host: api.pushbullet.com"));
+	ipClient.println(F("User-Agent: ESP8266"));
+	ipClient.println(F("Accept: */*"));
+	ipClient.println(F("Content-Type: application/json"));
+	ipClient.print(F("Content-Length: "));
+	ipClient.println(strlen(messagePrefix) + strlen(messageContent) + 25);
+	ipClient.print(F("Access-Token: "));
+	ipClient.println(pushbulletToken);
+	ipClient.println();
+	ipClient.print(F("{\"body\":\""));
+	ipClient.print(messagePrefix);
+	ipClient.print(messageContent);
+	ipClient.print(F("\",\"type\":\"note\"}"));
+
+	// Waits for a response
+	unsigned long previousMillis = millis();
+	while (!ipClient.available()) {
+		dsc.loop();
+		if (millis() - previousMillis > 3000) {
+			Serial.println(F("Connection timed out waiting for a response."));
+			ipClient.stop();
+			return false;
+		}
+	}
+
+	// Reads the response until the first space - the next characters will be the HTTP status code
+	while (ipClient.available()) {
+		if (ipClient.read() == ' ') break;
+	}
+
+	// Checks the first character of the HTTP status code - the message was sent successfully if the status code
+	// begins with "2"
+	char statusCode = ipClient.read();
+
+	// Successful, reads the remaining response to clear the client buffer
+	if (statusCode == '2') {
+		while (ipClient.available()) ipClient.read();
+		ipClient.stop();
+		return true;
+	}
+
+	// Unsuccessful, prints the response to serial to help debug
+	else {
+		Serial.println(F("Push notification error, response:"));
+		Serial.print(statusCode);
+		while (ipClient.available()) Serial.print((char)ipClient.read());
+		Serial.println();
+		ipClient.stop();
+		return false;
+	}
+}
+
+
+void appendPartition(byte sourceNumber, char* messageContent) {
+	char partitionNumber[2];
+	itoa(sourceNumber + 1, partitionNumber, 10);
+	strcat(messageContent, partitionNumber);
 }
