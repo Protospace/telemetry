c794ceed7320399ec92d3e65f8e09368674194cd
Patrick Spencer <30306028+pspencer53@users.noreply.github.com>
2024-04-02 22:32:26 -0600

    Removed hard-coded path from batch file.
    Note that to run this script, you need to set the properties of any shortcut to the current directory of the script.

--- logon_tracker/logon_tracker.cmd
+++ logon_tracker/logon_tracker.cmd
@@ -1,2 +1,2 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted"
-PowerShell -file "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\logon_tracker.ps1"
+PowerShell -file "logon_tracker.ps1"
