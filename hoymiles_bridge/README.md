048d23a70906612631e6346b8cc229b54d7faa18
Tanner Collin <git@tannercollin.com>
2024-08-11 06:35:01 +0100

    Add Hoymiles solar production data bridge

---
+++ hoymiles_bridge/README.md
@@ -0,0 +1,70 @@
+# Hoymiles Solar Bridge
+
+This bridges solar production data between the Hoymiles Solar DTU and the member portal Spaceport.
+
+Runs on a Raspberry Pi Zero W on the solar user's home network.
+
+# Setup
+
+Install on Debian 11 / 12:
+
+```
+$ cp secrets.py.example secrets.py
+$ vim secrets.py
+```
+
+Set up Python:
+
+```
+$ sudo apt install python3 python3-pip python3-virtualenv
+$ virtualenv -p python3 env
+$ . env/bin/activate
+(env) $ pip install -r requirements.txt
+```
+
+Test running:
+
+```
+(env) $ python main.py
+```
+
+Persist with supervisor:
+
+```
+$ sudo apt install supervisor
+$ sudo touch /etc/supervisor/conf.d/hoymiles_bridge.conf
+$ sudoedit /etc/supervisor/conf.d/hoymiles_bridge.conf
+```
+
+Edit the file:
+
+```
+[program:hoymiles_bridge]
+user=tanner
+directory=/home/tanner/telemetry/hoymiles_bridge
+command=/home/tanner/telemetry/hoymiles_bridge/env/bin/python -u main.py
+stopsignal=INT
+stopasgroup=true
+killasgroup=true
+autostart=true
+autorestart=true
+stderr_logfile=/var/log/hoymiles_bridge.log
+stderr_logfile_maxbytes=10MB
+stdout_logfile=/var/log/hoymiles_bridge.log
+stdout_logfile_maxbytes=10MB
+```
+
+Reread the config:
+
+```
+$ sudo supervisorctl reread; sudo supervisorctl update
+```
+
+You can control it with these commands:
+
+```
+$ sudo supervisorctl status hoymiles_bridge
+$ sudo supervisorctl stop hoymiles_bridge
+$ sudo supervisorctl start hoymiles_bridge
+$ sudo supervisorctl restart hoymiles_bridge
+```
---
+++ hoymiles_bridge/main.py
@@ -0,0 +1,77 @@
+import os, sys
+import logging
+DEBUG = os.environ.get('DEBUG')
+logging.basicConfig(stream=sys.stdout,
+    format='[%(asctime)s] %(levelname)s %(module)s/%(funcName)s - %(message)s',
+    level=logging.DEBUG if DEBUG else logging.INFO)
+
+import time
+import json
+
+import asyncio
+import aiohttp
+from hoymiles_wifi.dtu import DTU
+
+import secrets
+
+async def process_solar_data(data):
+    try:
+        serial = data.device_serial_number
+        power = int(data.active_power)
+        name = secrets.SOLAR_INFO['name']
+
+        logging.info('DTU Serial: %s, user: %s, power: %s', serial, name, power)
+
+        if power < 5:
+            logging.info('Forcing power to zero, night time')  # DTU reports 2 W at night??
+            power = 0
+
+        async with aiohttp.ClientSession() as session:
+            data = dict(user=name, power=power)
+
+            if DEBUG:
+                url = 'https://api.spaceport.dns.t0.vc/stats/solar_data/'
+            else:
+                url = 'https://api.my.protospace.ca/stats/solar_data/'
+
+            await session.post(url, json=data, timeout=10)
+
+            logging.info('Sent to portal URL: %s', url)
+    except BaseException as e:
+        logging.error('Problem sending json to portal:')
+        logging.exception(e)
+
+async def get_hoymiles_data():
+    try:
+        dtu = DTU(secrets.SOLAR_INFO['dtu_ip'])
+        response = await dtu.async_get_real_data_new()
+
+        if response:
+            logging.info('DTU response: %s', ', '.join(str(response).splitlines()[:4]))
+            logging.debug('DTU response: %s', response)
+            return response
+        else:
+            raise
+    except BaseException as e:
+        logging.error('Problem getting data from DTU:')
+        logging.exception(e)
+        return False
+
+async def main():
+    while True:
+        data = await get_hoymiles_data()
+        if data:
+            await process_solar_data(data)
+        else:
+            logging.info('Bad data.')
+
+        await asyncio.sleep(180)
+
+
+if __name__ == '__main__':
+    logging.info('')
+    logging.info('==========================')
+    logging.info('Booting up...')
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
