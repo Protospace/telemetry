c518aba0ee353337c409914de81f787017a77bb1
Tanner Collin <git@tannercollin.com>
2024-12-10 23:22:35 +0000

    Serve p1s chamber image from /name/pic

--- bamboo_tracker/main.py
+++ bamboo_tracker/main.py
@@ -8,22 +8,26 @@ DEBUG = os.environ.get('DEBUG')
 logging.basicConfig(
         format='[%(asctime)s] %(levelname)s %(module)s/%(funcName)s - %(message)s',
         level=logging.DEBUG if DEBUG else logging.INFO)
+logging.getLogger('aiohttp').setLevel(logging.DEBUG if DEBUG else logging.WARNING)
 
 import time
 import json
 import asyncio
 import aiohttp
+from aiohttp import web
 import sys
+
+app = web.Application()
 
 from pybambu import BambuClient
 
-if len(sys.argv) != 5:
+if len(sys.argv) != 6:
     print('Missing arguments. Run like so:')
-    print('python main.py NAME SERIAL PRINTER_IP ACCESS_CODE')
-    print('Example: python main.py p1s1 123412341234123 172.17.18.999 12345679')
+    print('python main.py NAME SERIAL PRINTER_IP ACCESS_CODE LISTEN_PORT')
+    print('Example: python main.py p1s1 123412341234123 172.17.18.999 12345679 8080')
     exit(1)
 
-_, NAME, SERIAL, PRINTER_IP, ACCESS_CODE = sys.argv
+_, NAME, SERIAL, PRINTER_IP, ACCESS_CODE, LISTEN_PORT = sys.argv
 
 client = BambuClient(
     device_type='P1S',
@@ -37,6 +41,7 @@ printer = dict(
     info=dict(),
     temperature=dict(),
 )
+
 
 def event_handler(event):
     if 'jpeg_received' in event:
@@ -80,8 +85,25 @@ async def portal_send():
 
             logging.debug('Done sending.')
 
+async def index(request):
+    return web.Response(text='bambu bridge', content_type='text/html')
+
+async def pic(request):
+    return web.Response(body=client._device.p1p_camera.get_jpeg(), content_type='image/jpeg')
+
+
+async def main():
+    app.router.add_get('/', index)
+    app.router.add_get('/' + NAME + '/pic', pic)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', LISTEN_PORT)
+    await site.start()
 
 loop = asyncio.get_event_loop()
 loop.create_task(listen_printer())
 loop.create_task(portal_send())
+loop.create_task(main())
 loop.run_forever()
+
