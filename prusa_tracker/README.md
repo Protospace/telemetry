9fb7ab7ce187290a1ab0d7e82568131026714cdc
Tanner Collin <git@tannercollin.com>
2025-10-10 00:08:24 +0000

    Add prusa tracker script

---
+++ prusa_tracker/README.md
@@ -0,0 +1,78 @@
+# Prusa Printer Tracker
+
+
+### Sample Data
+
+Idle:
+
+```
+    "sensor.prusa_xl": "idle",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "unavailable",
+    "sensor.prusa_xl_filename": "unavailable",
+    "sensor.prusa_xl_print_start": "unavailable",
+    "sensor.prusa_xl_print_finish": "unavailable"
+```
+
+Attention?
+
+```
+    "sensor.prusa_xl": "attention",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "0.0",
+    "sensor.prusa_xl_filename": "Z_thread_greaser_0.4n_0.2mm_PETG_XLIS_13m.bgcode",
+    "sensor.prusa_xl_print_start": "2025-10-09T19:33:23+00:00",
+    "sensor.prusa_xl_print_finish": "unavailable"
+```
+
+Printing (bed heating):
+
+```
+    "sensor.prusa_xl": "printing",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "0.0",
+    "sensor.prusa_xl_filename": "Z_thread_greaser_0.4n_0.2mm_PETG_XLIS_13m.bgcode",
+    "sensor.prusa_xl_print_start": "2025-10-09T22:03:23+00:00",
+    "sensor.prusa_xl_print_finish": "2025-10-09T22:16:49+00:00"
+```
+
+Actually printing:
+
+```
+    "sensor.prusa_xl": "printing",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "1.0",
+    "sensor.prusa_xl_filename": "Z_thread_greaser_0.4n_0.2mm_PETG_XLIS_13m.bgcode",
+    "sensor.prusa_xl_print_start": "2025-10-09T22:03:23+00:00",
+    "sensor.prusa_xl_print_finish": "2025-10-09T22:26:19+00:00"
+```
+
+100% progress:
+
+```
+    "sensor.prusa_xl": "printing",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "100.0",
+    "sensor.prusa_xl_filename": "Z_thread_greaser_0.4n_0.2mm_PETG_XLIS_13m.bgcode",
+    "sensor.prusa_xl_print_start": "2025-10-09T22:03:23+00:00",
+    "sensor.prusa_xl_print_finish": "2025-10-09T22:28:19+00:00"
+```
+
+Finished:
+
+```
+    "sensor.prusa_xl": "finished",
+    "sensor.prusa_xl_print_speed": "100",
+    "sensor.prusa_xl_material": "---",
+    "sensor.prusa_xl_progress": "unavailable",
+    "sensor.prusa_xl_filename": "unavailable",
+    "sensor.prusa_xl_print_start": "unavailable",
+    "sensor.prusa_xl_print_finish": "unavailable"
+```
+
+
---
+++ prusa_tracker/main.py
@@ -0,0 +1,109 @@
+# Script that gets Prusa XL printer status from Home Assistant
+# and then sends to Spaceport.
+
+import os, logging
+DEBUG = os.environ.get('DEBUG')
+logging.basicConfig(
+        format='[%(asctime)s] %(levelname)s %(module)s/%(funcName)s - %(message)s',
+        level=logging.DEBUG if DEBUG else logging.INFO)
+logging.getLogger('aiohttp').setLevel(logging.DEBUG if DEBUG else logging.WARNING)
+
+import time
+import json
+import asyncio
+import aiohttp
+import sys
+
+import secrets
+
+
+if len(sys.argv) != 2:
+    print('Missing arguments. Run like so:')
+    print('python main.py NAME')
+    print('Example: python main.py prusa_xl')
+    exit(1)
+
+_, NAME, = sys.argv
+
+printer = dict(
+    info=dict(),
+)
+
+async def printer_status():
+    while True:
+        sleep_time = 5 if DEBUG else 60
+        await asyncio.sleep(sleep_time)
+
+        try:
+            headers = {'Authorization': 'Bearer ' + secrets.HOMEASSISTANT_TOKEN}
+            url = secrets.HOMEASSISTANT_URL + '/api/states'
+            async with aiohttp.ClientSession() as session:
+                res = await session.get(url, headers=headers, timeout=10)
+                res.raise_for_status()
+                res = await res.json()
+        except KeyboardInterrupt:
+            break
+        except BaseException as e:
+            printer['info']['online'] = False
+            logging.error('Problem getting status from Home Assistant URL %s:', url)
+            logging.exception(e)
+
+        status = {}
+        prefix = 'sensor.' + NAME   # sensor.prusa_xl
+
+        for entry in res:
+            if entry['entity_id'].startswith(prefix):
+                status[entry['entity_id']] = entry['state']
+
+        logging.debug('Status data:\n%s', json.dumps(status, indent=4))
+
+        if not status:
+            raise Exception('Status empty, do you have the correct serial number?')
+
+        printer['info']['online'] = True
+        printer['info']['state'] = status['sensor.prusa_xl'].title()
+        printer['info']['print_speed'] = status['sensor.prusa_xl_print_speed']
+        printer['info']['material'] = status['sensor.prusa_xl_material']
+        printer['info']['progress'] = status['sensor.prusa_xl_progress']
+        printer['info']['filename'] = status['sensor.prusa_xl_filename']
+        printer['info']['print_start'] = status['sensor.prusa_xl_print_start']
+        printer['info']['print_finish'] = status['sensor.prusa_xl_print_finish']
+
+        logging.info('Sending to portal...')
+        logging.debug('JSON data:\n%s', json.dumps(printer, indent=4))
+
+        try:
+            url = 'https://api.my.protospace.ca/stats/{}/printer3d/'.format(NAME)
+            async with aiohttp.ClientSession() as session:
+                await session.post(url, json=printer, timeout=10)
+        except KeyboardInterrupt:
+            break
+        except BaseException as e:
+            logging.error('Problem sending printer data to portal %s:', url)
+            logging.exception(e)
+
+        try:
+            url = 'https://api.spaceport.dns.t0.vc/stats/{}/printer3d/'.format(NAME)
+            async with aiohttp.ClientSession() as session:
+                await session.post(url, json=printer, timeout=10)
+        except KeyboardInterrupt:
+            break
+        except BaseException as e:
+            logging.info('Problem sending printer data to dev portal %s:', url)
+
+        logging.debug('Done sending.')
+
+
+
+def task_died(future):
+    if os.environ.get('SHELL'):
+        logging.error('Bambu tracker task died!')
+    else:
+        logging.error('Bambu tracker task died! Waiting 60s and exiting...')
+        time.sleep(60)
+    exit()
+
+loop = asyncio.get_event_loop()
+loop.create_task(printer_status()).add_done_callback(task_died)
+loop.run_forever()
+
